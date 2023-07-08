1d94c8fc4d0260c064a15d3ec92137bb84806137
Kay-YOSHI <ksk0110@gmail.com>
2023-07-08 13:35:50 +0900

    streamlitお試し

--- strike_zone_estimation.py
+++ strike_zone_estimation.py
@@ -7,21 +7,69 @@ from plotnine import *
 from dfply import *
 import math
 
-st.write('# MLBにおけるストライクゾーンの推定＆可視化')
+st.write("# MLBにおけるストライクゾーンの推定＆可視化")
 
-st.write('## データ取得')
+st.write("## データ取得")
 
 # Statcast pitch-by-pitch data on all regular season games in 2019
-mlb2019 = statcast(start_dt = '2019-03-28', end_dt = '2019-09-29')
+mlb2019 = statcast(start_dt="2019-03-28", end_dt="2019-04-30")
 
 # 欠損値落としとく
-mlb = \
-    mlb2019 >> \
-    filter_by(X.plate_x.notnull()) >> \
-    filter_by(X.plate_z.notnull()) >> \
-    filter_by(X.sz_top.notnull()) >> \
-    filter_by(X.sz_bot.notnull())
+mlb = (
+    mlb2019
+    >> filter_by(X.plate_x.notnull())
+    >> filter_by(X.plate_z.notnull())
+    >> filter_by(X.sz_top.notnull())
+    >> filter_by(X.sz_bot.notnull())
+)
 
 # 概観
+st.write(mlb.shape)
 st.write(mlb.head())
 
+st.write("## 投球座標の分布")
+
+# 見逃しかつファストボールに絞る＆重いのでランダムサンプリングしとく
+dist = (
+    mlb
+    >> filter_by(X.pitch_type == "FF")
+    >> filter_by((X.description == "called_strike") | (X.description == "ball"))
+    >> mutate(code=if_else(X.description == "called_strike", "Called Strike", "Ball"))
+    >> sample(n=10000)
+)
+
+# ルールブック上のストライクゾーン
+# 上限と下限は打者平均とする．なお，ホームプレート＝17インチ，ボール＝9インチ（1インチ＝1/12フィート）．
+plate_width = (17 + 2 * (9 / math.pi)) / 12
+xm = [
+    -(plate_width / 2),
+    plate_width / 2,
+    plate_width / 2,
+    -(plate_width / 2),
+    -(plate_width / 2),
+]
+zm = [
+    dist["sz_bot"].mean(),
+    dist["sz_bot"].mean(),
+    dist["sz_top"].mean(),
+    dist["sz_top"].mean(),
+    dist["sz_bot"].mean(),
+]
+sz_mlb = pd.DataFrame({"xm": xm, "zm": zm})
+
+# 可視化
+pdist = (
+    ggplot(dist, aes(x="plate_x", y="plate_z"))
+    + geom_point(aes(color="code"), alpha=1.0)
+    + geom_path(sz_mlb, aes(x="xm", y="zm"), linejoin="round", size=1.0)
+    + coord_equal()
+    + scale_x_continuous(name="Horizontal location (ft.)", limits=(-2.0, 2.0))
+    + scale_y_continuous(name="Vertical location (ft.)", limits=(0.0, 5.0))
+    + labs(
+        title="Pitch distribution",
+        subtitle="(Randomly sampled 10,000 obs.)",
+        caption="*Catcher's perspective.",
+    )
+    + theme_bw()
+)
+st.pyplot(ggplot.draw(pdist))
